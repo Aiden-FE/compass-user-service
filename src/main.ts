76f5480ca944e775f97323fbb4d26956bef4dfdb
Aiden-FE <Aiden_FE@outlook.com>
2023-09-26 14:02:29 +0800

    fix: set host

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
-import { Transport } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.TCP,
     options: {
+      host: '0.0.0.0',
       port: 3001,
     },
   });
 
-  app.listen().then(() => Logger.log('User service is listening'));
+  app.listen().then(() => Logger.log('User service is listening on 0.0.0.0:3001/TCP port'));
 }
 
 bootstrap();
